b8f015b4941946f9374a6dd53e74633e5d2d79f5
anastasia <anyasirman@outlook.com>
2022-08-30 16:11:53 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+
+
+
+m1 = Movie.create(title: "Top Gun", description: "This is a movie with Tom Cruz and fast planes", rating: 'PG-13', runtime: '90 minutes', release_date: '20220819', genre: 'Action')
+m2 = Movie.create(title: "Spiderman: No way home", description: "This is a movie with Tom Holland and Zendaya", rating: 'PG-13', runtime: '95 minutes', release_date: '20220819', genre: 'Action')
+m3 = Movie.create(title: "DC league of super pets", description: "This is a movie with annimated pets trying to save superman", rating: 'PG', runtime: '76 minutes', release_date: '20220820', genre: 'Family')
+m4 = Movie.create(title: "Barbarian", description: "This is a movie about sometihing", rating: 'R', runtime: '104 minutes', release_date: '20210820', genre: 'Horror')
+m5 = Movie.create(title: "Bullet Train", description: "This is a movie about a bullet train", rating: 'R', runtime: '204 minutes', release_date: '202108556', genre: 'Action')
+
