9ad490b89a901edf40f8d4b96145790a8d7aa5ab
anastasia <anyasirman@outlook.com>
2022-08-31 16:20:38 -0400

    added context to checkout 1

--- client/src/CheckoutStage1.js
+++ client/src/CheckoutStage1.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react'
 import ShowtimeButton from './ShowtimeButton'
+import { useContext } from 'react'
+import { MovieContext } from './App'
 
 const CheckoutStage1 = () => {
+
+    const {globalMovie, setGlobalMovie} = useContext(MovieContext)
     const [allTheaters, setAllTheaters] = useState([])
     const [theater, setTheater] = useState({})
     const [movie, setMovie] = useState({})
     const [showtimes, setShowtimes] = useState([])
-
+console.log(globalMovie)
     // show method is technically not needed if we are fetching all theaters, it depends
     // useEffect(() => {
     //     const getTheater = async (theaterId = 1) => {
@@ -18,20 +22,20 @@ const CheckoutStage1 = () => {
     //     getTheater()
     // }, [])
 
-    useEffect(() => {
-        const getAllTheaters = async (theaterId = 1) => {
-            let req = await fetch('http://localhost:3000/theaters')
-            let res = await req.json()
-            console.log('theaters', res)
-            setAllTheaters(res)
-            setTheater(res[theaterId - 1])
-        }
-        getAllTheaters()
-    }, [])
+    // useEffect(() => {
+    //     const getAllTheaters = async (theaterId = 1) => {
+    //         let req = await fetch('http://localhost:3000/theaters')
+    //         let res = await req.json()
+    //         console.log('theaters', res)
+    //         setAllTheaters(res)
+    //         setTheater(res[theaterId - 1])
+    //     }
+    //     getAllTheaters()
+    // }, [])
 
     useEffect(() => {
-        const getMovie = async (movieId = 1) => {
-            let req = await fetch(`http://localhost:3000/movies/${movieId}`)
+        const getMovie = async () => {
+            let req = await fetch(`http://localhost:3000/movies/${globalMovie.movie.id}`)
             let res = await req.json()
             console.log('movie', res)
             setMovie(res)
